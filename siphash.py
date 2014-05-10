ed63af4c8dd04151fd59478c7893a47e1294ce78
Philipp Jovanovic <philipp@jovanovic.io>
2014-05-10 09:19:42 +0200

    moved from 2-space to 4-space intendation

--- siphash.py
+++ siphash.py
@@ -1,72 +1,73 @@
 class SipHash:
-  def __init__(self,c=2,d=4):
-    assert c >= 0 
-    assert d >= 0 
-    self._c = c 
-    self._d = d
-    self._mask = 0xffffffffffffffff
 
-  def __call__(self,m,k):
-    assert k >= 0 and k < (1 << 128)
-    self._k = [k & self._mask, k >> 64]
+    def __init__(self,c=2,d=4):
+        assert c >= 0
+        assert d >= 0
+        self._c = c
+        self._d = d
+        self._mask = 0xffffffffffffffff
 
-    # initialization
-    self._v = [self._k[0] ^ 0x736f6d6570736575, self._k[1] ^ 0x646f72616e646f6d,
-               self._k[0] ^ 0x6c7967656e657261, self._k[1] ^ 0x7465646279746573]
+    def __call__(self,m,k):
+        assert k >= 0 and k < (1 << 128)
+        self._k = [k & self._mask, k >> 64]
 
-    self._parse_msg(m)
+        # initialization
+        self._v = [self._k[0] ^ 0x736f6d6570736575, self._k[1] ^ 0x646f72616e646f6d,
+                   self._k[0] ^ 0x6c7967656e657261, self._k[1] ^ 0x7465646279746573]
 
-    # compression
-    for i in xrange(len(self._msg)):
-      self._v[3] ^= self._msg[i]
-      for j in xrange(self._c):
-        self._sip_round()
-      self._v[0] ^= self._msg[i]
-    
-    # finalization
-    self._v[2] ^= 0xff
-    for i in xrange(self._d):
-      self._sip_round()
+        self._parse_msg(m)
 
-    return self._v[0] ^ self._v[1] ^ self._v[2] ^ self._v[3]
+        # compression
+        for i in xrange(len(self._msg)):
+            self._v[3] ^= self._msg[i]
+            for j in xrange(self._c):
+                self._sip_round()
+            self._v[0] ^= self._msg[i]
 
-  def _parse_msg(self,m):
-    l = len(m)
-    self._msg = []
-    n = (l//8)*8
+        # finalization
+        self._v[2] ^= 0xff
+        for i in xrange(self._d):
+            self._sip_round()
 
-    # parse m_0 to m_{l-2}
-    for i in xrange(0, n, 8):
-      s = ord(m[i])
-      for j in xrange(1,8):
-        s |= ord(m[i+j]) << 8*j
-      self._msg.append(s)
+        return self._v[0] ^ self._v[1] ^ self._v[2] ^ self._v[3]
 
-    # parse m_{l-1}
-    s = (l % 256) << 56
-    for i in xrange(l-n):
-      s |= ord(m[n+i]) << 8*i
-    self._msg.append(s)
+    def _parse_msg(self,m):
+        l = len(m)
+        self._msg = []
+        n = (l//8)*8
 
-  def _rotl(self,v,r):
-    return ( ( ( v << r ) & self._mask) | ( v >> ( 64 - r ) ) ) 
+        # parse m_0 to m_{l-2}
+        for i in xrange(0, n, 8):
+            s = ord(m[i])
+            for j in xrange(1,8):
+                s |= ord(m[i+j]) << 8*j
+            self._msg.append(s)
 
-  def _sip_round(self):
-    self._v[0] += self._v[1] 
-    self._v[2] += self._v[3]
-    self._v[2] &= self._mask
-    self._v[0] &= self._mask
-    self._v[1] = self._rotl(self._v[1],13)
-    self._v[3] = self._rotl(self._v[3],16)
-    self._v[1] ^= self._v[0] 
-    self._v[3] ^= self._v[2] 
-    self._v[0] = self._rotl(self._v[0],32)
-    self._v[2] += self._v[1]
-    self._v[0] += self._v[3]
-    self._v[2] &= self._mask
-    self._v[0] &= self._mask
-    self._v[1] = self._rotl(self._v[1],17)
-    self._v[3] = self._rotl(self._v[3],21)
-    self._v[1] ^= self._v[2]
-    self._v[3] ^= self._v[0]
-    self._v[2] = self._rotl(self._v[2],32)
+        # parse m_{l-1}
+        s = (l % 256) << 56
+        for i in xrange(l-n):
+            s |= ord(m[n+i]) << 8*i
+        self._msg.append(s)
+
+    def _rotl(self,v,r):
+        return ( ( ( v << r ) & self._mask) | ( v >> ( 64 - r ) ) )
+
+    def _sip_round(self):
+        self._v[0] += self._v[1]
+        self._v[2] += self._v[3]
+        self._v[2] &= self._mask
+        self._v[0] &= self._mask
+        self._v[1] = self._rotl(self._v[1],13)
+        self._v[3] = self._rotl(self._v[3],16)
+        self._v[1] ^= self._v[0]
+        self._v[3] ^= self._v[2]
+        self._v[0] = self._rotl(self._v[0],32)
+        self._v[2] += self._v[1]
+        self._v[0] += self._v[3]
+        self._v[2] &= self._mask
+        self._v[0] &= self._mask
+        self._v[1] = self._rotl(self._v[1],17)
+        self._v[3] = self._rotl(self._v[3],21)
+        self._v[1] ^= self._v[2]
+        self._v[3] ^= self._v[0]
+        self._v[2] = self._rotl(self._v[2],32)
