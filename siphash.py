b884e2b31baacb21d3c77478107ccc7eae18ec53
Philipp Jovanovic <philipp@jovanovic.io>
2014-12-03 19:31:48 +0100

    simpler load/store of binary data

--- siphash.py
+++ siphash.py
@@ -5,6 +5,10 @@
    :copyright: (c) 2013-2014 by Philipp Jovanovic <philipp@jovanovic.io>.
    :license: MIT, see LICENSE for more details.
 """
+
+from struct import unpack as load
+from struct import pack as store
+
 class SipHash:
 
     def __init__(self,c=2,d=4,h=8):
@@ -21,7 +25,7 @@ class SipHash:
     def __call__(self,m,k):
         assert len(k) == 16
         msg = self.parse_msg(m)
-        key = [self.u8_to_u64(k[0:8]), self.u8_to_u64(k[8:16])]
+        key = [load('<Q', k[0:8])[0], load('<Q', k[8:16])[0]]
         tag = ''
 
         # initialization
@@ -44,22 +48,9 @@ class SipHash:
                 self.v[1] ^= self.ds[2]
             for j in xrange(self.d):
                 self.sip_round()
-            tag += self.u64_to_u8(self.v[0] ^ self.v[1] ^ self.v[2] ^ self.v[3])
+            tag += store('<Q', self.v[0] ^ self.v[1] ^ self.v[2] ^ self.v[3])
 
         return tag
-
-    def u8_to_u64(self,x):
-        return (ord(x[0]) <<  0) | \
-               (ord(x[1]) <<  8) | \
-               (ord(x[2]) << 16) | \
-               (ord(x[3]) << 24) | \
-               (ord(x[4]) << 32) | \
-               (ord(x[5]) << 40) | \
-               (ord(x[6]) << 48) | \
-               (ord(x[7]) << 56)
-
-    def u64_to_u8(self,x):
-        return b''.join([chr((x >> 8*i) & 0xff) for i in xrange(8)])
 
     def parse_msg(self,m):
         l = len(m)
